127297981339d620653b1b5c1c4efb3823df0240
yangx <yangxuilyx@hotmail.com>
2025-02-22 23:18:49 +0800

    更新

--- src/aspnetcore/FastXTpl.WebTemplate.Host/Models/Sms/GetPhoneCodeInput.cs
+++
@@ -1,9 +0,0 @@
-namespace FastXTpl.WebTemplate.Host.Models.Sms;
-
-public class GetPhoneCodeInput
-{
-    /// <summary>
-    /// 手机号
-    /// </summary>
-    public string PhoneNumber { get; set; }
-}
